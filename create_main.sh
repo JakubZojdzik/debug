6d6f969e6cb088a661f302ec6f1db333a6967c1c
Jakub Żojdzik <69974199+JakubZojdzik@users.noreply.github.com>
2022-07-20 01:39:59 +0200

    Upload files

---
+++ create_main.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp ~/Templates/main.cpp ./
+dt=`date +%d-%m-%Y`
+sed -i "s/--DATE--/${dt}/" ./main.cpp
+echo $dt
---
+++ main.cpp
@@ -0,0 +1,36 @@
+/*
+    Jakub Żojdzik
+    20-07-2022
+*/
+
+#include <bits/stdc++.h>
+#ifdef LOCAL
+#include "debug.hpp"
+#else
+#define debug(...)
+#endif
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define dt cout << "HERE!\n"
+#define nl cout << '\n'
+    
+int main()
+{
+    cout.tie(0);
+    cin.tie(0)->sync_with_stdio(0);
+
+}
+
+/*
+
+*/
