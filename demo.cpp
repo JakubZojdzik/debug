d26a53f35cd44a3554f953c8bfd0783f80baed4b
JakubZojdzik <jakub.zojdzik@gmail.com>
2024-01-24 23:03:49 +0100

    Add demo file

--- demo.cpp
+++ demo.cpp
@@ -1,5 +1,5 @@
 /*
-    --Your name--
+    -- Your name --
     --DATE--
 */
 
@@ -28,16 +28,39 @@ typedef vector<pair<long long, long long>> vpll;
 #define rep(i, x, y) for(ll i = (ll)x; i <= (ll)y; i++)
 #define all(x) x.begin(), x.end()
 #define sz(x) (ll)(x).size()
-#define nl '\n'
+#define nl cout << '\n'
 
 int main()
 {
     cout.tie(0);
     cin.tie(0)->sync_with_stdio(0);
 
-    
+    tuple<int, string, double> x;
+    get<0>(x) = 4;
+    get<1>(x) = "abrakadabra";
+    get<2>(x) = 3.14;
+    debug(x);
+
+    vpll y;
+    y.pb({20000000000, 1});
+    y.pb({3, 7});
+    y.pb({400000000, 200000000000000000});
+    debug(y);
+    debug(y[0].se);
+
+    priority_queue<int> pq;
+    pq.push(5);
+    pq.push(5);
+    pq.push(2);
+    pq.push(8);
+    pq.push(1);
+    debug(pq);
 }
 
 /*
-
+Given output:
+[DEBUG L:42] x = {4, "abrakadabra", 3.14}
+[DEBUG L:48] y = {{20000000000, 1}, {3, 7}, {400000000, 200000000000000000}}
+[DEBUG L:49] y[0].se = 1
+[DEBUG L:57] pq = {8, 5, 5, 2, 1}
 */
