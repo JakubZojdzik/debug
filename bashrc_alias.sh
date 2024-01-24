cdb6f3c0ef5204a62f4b8507bce6a043427524f0
JakubZojdzik <jakub.zojdzik@gmail.com>
2024-01-24 22:57:04 +0100

    Add DLOCAL flag to compile command

--- bashrc_alias.sh
+++ bashrc_alias.sh
@@ -1,10 +1,8 @@
-# Add this function at the end of your .bashrc or .zshrc file to create `c` alias compiling your program with standard competition flags
-
 c() {
     if [[ "$1" != "" ]]
     then
-        g++ -O3 -static "$1" -std=c++20 -o main
+        g++ -DLOCAL -O3 -static "$1" -std=c++20 -o main
     else
-        g++ -O3 -static main.cpp -std=c++20 -o main
+        g++ -DLOCAL -O3 -static main.cpp -std=c++20 -o main
     fi
 }
